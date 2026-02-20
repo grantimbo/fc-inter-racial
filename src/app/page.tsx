34187aaef85e2c7acf4b6753081a6fb4e44704b6
Grant Imbo <grant.imbo@gmail.com>
2026-02-21 01:34:01 +0800

    working on facebook feed feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import PlayersSection from "./components/players";
 import Footer from "./components/footer";
 import HeroSlider from "./components/slider";
 import OurStory from "./components/our-story";
+import FacebookFeed from "./components/facebook-feed";
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <HeroSlider/>
+      <FacebookFeed/>
       <PlayersSection/>
       <OurStory/>
       <Footer/>
